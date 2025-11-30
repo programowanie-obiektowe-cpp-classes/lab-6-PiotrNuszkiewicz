5e914c629a1dbf1ead8ecc0faebb5a87c6c2dff7
PiotrNuszkiewicz <01172090@pw.edu.pl>
2025-11-30 16:53:51 +0100

    plik foo

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,11 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
+    std::transform(people.begin(), people.end(), retval.rbegin(), [](Human& h) {
+        char status = h.isMonster() ? 'n' : 'y';
+        h.birthday();
+        return status;
     // Twoja implementacja tutaj
-
+    });
     return retval;
 }
